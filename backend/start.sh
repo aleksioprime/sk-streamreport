a1cba169fc99879f452b067f70e09cbb63be1d4f
Semochkin Aleksey <ASemochkin@sk.ru>
2023-02-16 00:04:51 +0300

    add start.sh loaddata

--- backend/start.sh
+++ backend/start.sh
@@ -3,5 +3,6 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate --no-input
+python manage.py loaddata data.json
 
 python manage.py runserver 0.0.0.0:8000
