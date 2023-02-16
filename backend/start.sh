f25d56bd58376d0198956350dd6b37d4885ee00b
Semochkin Aleksey <ASemochkin@sk.ru>
2023-02-16 18:51:37 +0300

    delete mkdir from start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -3,7 +3,7 @@
 python manage.py makemigrations
 python manage.py migrate --no-input
 
-mkdir static & mkdir media
+# mkdir static & mkdir media
 
 python manage.py collectstatic --no-input
 python manage.py loaddata data.json
