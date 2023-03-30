8554f87996dcc6e0b6949ccc980ab78726cd18c1
Kuzmina Anastasia <AKuzmina@sk.ru>
2023-03-31 00:45:19 +0300

    ширина табл, поправлены столбцы, инпуты - ?

--- frontend/src/views/AssessPeriodView.vue
+++ frontend/src/views/AssessPeriodView.vue
@@ -4,13 +4,13 @@
       <template v-slot:link><a href="/assessment">Вернуться к списку итоговых работ</a></template>
       <template v-slot:header>Журнал оценок за период</template>
     </base-header>
-    <table>
+    <table >
             <thead class="thead-dark text-center">
               <tr>
                 <th scope="col" class="text-center" rowspan="2">№</th>
                 <th scope="col" rowspan="2" style="width: 50%" >ФИО студента</th>
-                <th scope="col" colspan="4" style="width: 20%">Средние балы по критериям</th>
-                <th scope="col" colspan="3" style="width: 10%" >Итоговая оценка</th>
+                <th scope="col" colspan="5" style="width: 20%">Средние балы по критериям</th>
+                <th scope="col" colspan="4" style="width: 10%" >Итоговая оценка</th>
                 
               </tr>
               <tr>
@@ -18,9 +18,12 @@
                 <th style="width: 5%">B</th>
                 <th style="width: 5%">C</th>
                 <th style="width: 5%">D</th>
-                <th style="width: 10%">Сумма</th>
-                <th style="width: 10%">Макс </th>
-                <th style="width: 10%">Расчет </th>
+                <th style="width: 10%">Сумма/Макс</th>
+                <th style="width: 10%"> Оценка за ИКР </th>
+                <th style="width: 10%">Среднее текущее </th>
+                <th style="width: 10%">Итоговый расчет </th>
+                <th style="width: 10%">Итоговая оценка </th>
+
 
               </tr>
             </thead>
@@ -28,46 +31,35 @@
               <tr>
                 <th scope="row">1</th>
                 <td></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
+                <td> 
+                  1 
+                  2
+                  3
+                  Средняя:
+                  <input type="text" class="form-control"> </td>
+                <td>
+                  1 
+                  2
+                  3
+                  Средняя:
+                  <input type="text" class="form-control"></td>
+                <td>
+                  1 
+                  2
+                  3
+                  Средняя:
+                  <input type="text" class="form-control" ></td>
+                <td>
+                  1 
+                  2
+                  3
+                  Средняя:
+                  <input type="text" class="form-control" ></td>
+                <td></td>
                 <td><input type="text" class="form-control"></td>
                 <td></td>
                 <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td></td>
                 <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">4</th>
-                <td></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td><input type="text" class="form-control"></td>
-                <td></td>
-                <td></td>
-                <td></td>
               </tr>
             </tbody>
           </table>
@@ -105,10 +97,10 @@ export default {
 
 <style scoped>
 table {
-    width: 90%;
+    width: 100%;
     height: auto;
     border-collapse: collapse;
-    margin: auto auto;
+    margin: 0px 0px 8px;
   }
   td {
     padding: 5px;
