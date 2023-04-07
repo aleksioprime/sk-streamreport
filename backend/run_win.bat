158b732b221f481f635a471a7623be0fb8f6d1ff
Aleksei Semochkin <aleksioprime@gmail.com>
2023-04-08 02:24:19 +0300

    edit run files and fixtures locations

--- backend/run_win.bat
+++ backend/run_win.bat
@@ -1,10 +1,9 @@
 @echo on
 call "C:\venv\freshstream\Scripts\activate.bat"
-set POSTGRES_PORT=5432
+set POSTGRES_PORT=32768
 set POSTGRES_HOST=localhost
 set POSTGRES_DB=igskolkovo
 set POSTGRES_USER=igadmin
 set POSTGRES_PASSWORD=Pox{@K
-cd "D:\development\freshstream\backend"
 python manage.py runserver
 pause
